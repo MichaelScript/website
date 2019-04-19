64fbc299c00ed15145424e0eb56aa90d6409803d
Michael Green <me@michaelgreen.net>
2019-04-18 18:41:14 -0700

    basic layout stuff

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './common.css';
+class Card extends Component{
+    render(){
+        return (<div className="card">Hello World</div>)
+    }
+}
+export default Card;
---
+++ src/components/TopBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class TopBar extends Component{
+    render(){
+        return (
+            <div className="top-bar">TOP BAR</div>
+        )
+    }
+}
+
+export default TopBar;
