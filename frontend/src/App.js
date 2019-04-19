332205662fb4f49c1ba428068bb6b2a65d6f4c83
Michael Green <me@michaelgreen.net>
2019-04-19 10:08:05 -0700

    Title Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card.js'
 import TopBar from './components/TopBar.js';
+import TitleScreen from './components/TitleScreen.js'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <TopBar></TopBar>
+        <TitleScreen></TitleScreen>
         <Card></Card>
       </div>
     );
---
+++ frontend/src/components/TitleScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TitleScreen extends Component{
+    render(){
+        return (<div className="title-screen">
+            <h1 className="title">Michael Green</h1>
+            <h2 className="subtitle">Developer & Software Engineer</h2>
+        </div>)
+    }
+}
+
+export default TitleScreen;
