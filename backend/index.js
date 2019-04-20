3d2b3340562d1a2931494e518aad5dad459b394d
Michael Green <me@michaelgreen.net>
2019-04-20 13:59:12 -0700

    postgres starting in foreground

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,22 @@ const path = require('path');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '200mb'}));
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// const { Client } = require('pg')
+// const client = new Client()
 
+// client.connect().then(function(){
+
+// });
+// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+// console.log(res.rows[0].message) // Hello world!
+// await client.end()
+app.post('/hello',function(req,res){
+    console.log("hello");
+})
 const routes = {
     "auth": require(path.join(__dirname, "/app/auth"))(app)
 }
+
 
 // app.use('/auth/',routes['auth']);
 // console.log("Hello world");
