d5def3d645b3b7ecb588a5c732b861e6c41b9cab
Michael Green <me@michaelgreen.net>
2019-04-23 20:34:42 -0700

    Switching to mysql because postgres and docker werent getting along

--- backend/app/index.js
+++ backend/app/index.js
@@ -7,13 +7,23 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '200mb'}));
-
-const { Client } = require('pg')
-const client = new Client()
-
-client.connect().then(function(){
-    console.log('hello world');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'mysql',
+  user     : 'user',
+  password : 'password',
+  database : 'db'
 });
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
 // const res = await client.query('SELECT $1::text as message', ['Hello world!'])
 // console.log(res.rows[0].message) // Hello world!
 // await client.end()
