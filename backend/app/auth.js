ef713644fa11f2ba9f0fb936c75f2b703c253289
Michael Green <me@michaelgreen.net>
2019-04-23 21:30:59 -0700

    Password hashing

--- backend/app/auth.js
+++ backend/app/auth.js
@@ -1,33 +1,50 @@
 const express = require('express');
-
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 12;
 module.exports = function(app,db){
     const router = express.Router();
-
-    let createUser = (req,res)=>{
-        let user = req.body;
-        db.query(`
-        INSERT INTO users (email,password)
-        VALUES ('${user.email}','${user.password}')`,
-        function(err,results,fields){
-            if(!err){
-                console.log("results: ", results);
-                console.log("fields: ", fields);
-            } else{
-                console.error(err);
-            }
-        })
-    }
-    router.post('/createUser',createUser);
-
-
-    /* Create User Test */
-    const user = {
+     /* Test User */
+     const user = {
         "body":{
             "email":"me@michaelgreen.net",
             "password":"password"
         }
     }
-    // createUser(user);
+
+    let createUser = (req,res)=>{
+        let user = req.body;
+        /* Hashing */
+        bcrypt.hash(user.password,SALT_ROUNDS,(err,hash)=>{
+            if(!err){
+                db.query(`
+                INSERT INTO users (email,password)
+                VALUES ('${user.email}','${hash}')`,
+                (err,results,fields)=>{
+                    if(!err){
+                        console.log("results: ", results);
+                        console.log("fields: ", fields);
+                    } else{
+                        console.error(err);
+                    }
+                })        
+            }
+        })
+    }
+    createUser(user);
+    router.post('/createUser',createUser);
+
+    let deleteUser = (req,res)=>{
+        let user = req.body;
+        db.query(`DELETE FROM users WHERE email = '${user.email}'`,
+        function(err,results,fields){
+            if(!err){
+                console.log('Results: ', results);
+            } else{
+                console.error(err);
+            }
+        })
+    }
+    // deleteUser(user);
 
     let login = (req,res)=>{
         let user = req.body;
@@ -36,11 +53,18 @@ module.exports = function(app,db){
         `,
         function(err,results,fields){
             console.log("results: ",results);
-            let foundUser = results[0];
-            if(user.password == foundUser.password){
-                console.log("Succesful login");
-            } else{
-                console.log("Wrong password");
+            if(results.length > 0){
+                let foundUser = results[0];
+                console.log('user password is: ', user.password);
+                console.log('found user password is: ', foundUser.password);
+                /* Comparing the user provided password hash to the hash in the db */
+                bcrypt.compare(user.password, foundUser.password, function(err, matched) {
+                    if(matched){
+                        console.log("Succesful login");
+                    } else{
+                        console.log("Wrong password");
+                    }
+                });
             }
         })
     }
